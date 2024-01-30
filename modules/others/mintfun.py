d3fab97b6c808bbd4bbc94e7fea2b3833ac10b19
askaer <realaskaer@gmail.com>
2024-01-30 22:00:11 +0300

    Update mintfun.py

--- modules/others/mintfun.py
+++ modules/others/mintfun.py
@@ -1,7 +1,10 @@
 import random
 
+from web3.exceptions import Web3ValidationError
+
 from modules import Minter, Logger
 from config import MINTFUN_ABI
+from modules.interfaces import SoftwareException
 from utils.tools import helper, gas_checker
 from settings import MINTFUN_CONTRACTS
 
@@ -17,7 +20,7 @@ class MintFun(Minter, Logger):
 
         nft_contract, mint_price = random.choice(list(MINTFUN_CONTRACTS.items()))
 
-        contract = self.client.get_contract(nft_contract, MINTFUN_ABI[1])
+        contract = self.client.get_contract(self.client.w3.to_checksum_address(nft_contract), MINTFUN_ABI[1])
 
         try:
             nft_name = await contract.functions.name().call()
@@ -28,13 +31,22 @@ class MintFun(Minter, Logger):
 
         data = [self.client.address, 1, None]
 
+        tx_params = await self.client.prepare_transaction(value=mint_price)
+        result = False
+        counter = 0
+
         for index, item in enumerate(data, 1):
             try:
+                counter += 1
                 contract = self.client.get_contract(nft_contract, MINTFUN_ABI[index])
-                transaction = await contract.functions.mint(
-                    item
-                ).build_transaction(await self.client.prepare_transaction(value=mint_price))
+                if item:
+                    transaction = await contract.functions.mint(item).build_transaction(tx_params)
+                else:
+                    transaction = await contract.functions.mint().build_transaction(tx_params)
 
-                return await self.client.send_transaction(transaction)
-            except:
-                pass
+                result = await self.client.send_transaction(transaction)
+            except Web3ValidationError:
+                if counter == 3:
+                    raise SoftwareException('This mint do not support in software. You need "Mint NFT" function!')
+
+        return result
