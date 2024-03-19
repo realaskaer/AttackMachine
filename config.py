7acbed88562815cf13b8a085408a6b93627282f1
askaer <realaskaer@gmail.com>
2024-03-19 23:19:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -365,6 +365,7 @@ TOKENS_PER_CHAIN = {
     "Polygon":{
         'MATIC'             : "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
         'WMATIC'            : "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+        'ETH'               : "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
         'WETH'              : "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
         'USDT'              : "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
         'USDC'              : "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
