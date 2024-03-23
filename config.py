ed9f083ce7e868173e67ab2c4b7b1875ad82fc36
askaer <realaskaer@gmail.com>
2024-03-24 00:08:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -413,6 +413,7 @@ TOKENS_PER_CHAIN = {
         "USDC"              : "0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4",
     },
     "BNB Chain":{
+        "BNB"               : "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
         "USDT"              : "0x55d398326f99059fF775485246999027B3197955",
         "USDC"              : "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
     },
