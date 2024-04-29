f510721db91fa5515ef5fa3d6fb3afccaab3cec3
askaer <realaskaer@gmail.com>
2024-04-29 14:12:32 +0300

    Update client.py

--- modules/client.py
+++ modules/client.py
@@ -51,7 +51,7 @@ class Client(Logger):
             if proxy else TCPConnector(verify_ssl=False)
         )
 
-        self.request_kwargs = {"proxy": f"http://{proxy}"} if proxy else {}
+        self.request_kwargs = {"proxy": f"http://{proxy}", "verify_ssl": False} if proxy else {"verify_ssl": False}
         self.rpc = random.choice(network.rpc)
         self.w3 = AsyncWeb3(AsyncHTTPProvider(self.rpc, request_kwargs=self.request_kwargs))
         self.account_name = str(account_name)
